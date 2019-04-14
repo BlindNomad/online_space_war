094cba88da61f67fd272164399134a4e5cbce75e
Thiago J Silveira <blind.nomad@gmail.com>
2019-04-14 16:14:24 -0300

    rand

--- server/space_war/main.cpp
+++ server/space_war/main.cpp
@@ -14,6 +14,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <condition_variable>
+#include <time.h>
 
 #include "TcpListen.h"
 #include "Player.h"
@@ -29,6 +30,8 @@ int main(int argc, char** argv) {
     int ret;
     mutex mtx;
     Players players;
+    
+    srand(time(0));
 
     printf("Iniciando o servidor\n");
 
